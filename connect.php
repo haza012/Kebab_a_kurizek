96d6710a4a974165bd6429caec1e70149c958234
haza012 <115024970+haza012@users.noreply.github.com>
2022-12-01 16:11:42 +0100

    Delete connect.php

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "haza01";
-    $password = "Tis*763109";
-    $dbname = "haza01";
-    $spojeni = mysqli_connect($servername, $username, $password, $dbname);
-    mysqli_set_charset($spojeni, "utf8");
-?>
